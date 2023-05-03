8caca13dbaff19a2ce4968d71551216e69f98422
Amani <88464204+slurpinpuffs@users.noreply.github.com>
2023-05-02 20:18:13 -0700

    Update BillReader.py

--- BillReader.py
+++ BillReader.py
@@ -1,31 +1,90 @@
-import PyPDF2
+import openpyxl
 from openpyxl import Workbook
-import re
+import os
 
-file = input("Please enter the filepath for the bill you'd like to input (no quotation marks): ")
-folder = input("Please enter the filepath for the folder you'd like the outputted sheet to go to (no quotation marks, do not end with slash): ")
-fileName = input("Please input a name for the output file (no spaces): ")
-page_list = []
-reader = PyPDF2.PdfReader(file)
-count = len(reader.pages)
+print("Hi! Welcome to the Bill Reader, an SMS Fisher tool by Amani Medcroft.")
+print("Please note that this tool requires the catalog to be in a very specific format.")
+print("Please check your bill spreadsheet to make sure that the format is correct.")
+print("If you encounter any bugs, issues, or have any complaints/suggestions, just let me know!")
+print()
+file = input("Please enter the filepath for the catalog spreadsheet (.xlsx file) you'd like to input: ")
+file = file.replace('"', '')
+exchRate = float(input("What is the exchange rate today? (1.00 EUR = X.XX USD): "))
 
-workbook = Workbook()
-sheet = workbook.active
-row = 1
+splitFile = file.split('\\')
+splitFile = splitFile[-1].split('.')
+fileName = splitFile[0] + '_Fishbowl'
+folder = os.path.expanduser('~\Downloads')
+bill = openpyxl.load_workbook(file, data_only=True)
+billSheet = bill.active
+newWorkbook = Workbook()
+newSheet = newWorkbook.active
+maxRow = billSheet.max_row
+billRow = 1
+newRow = 2
 
-for page_number in range(count):
-    page = reader.pages[page_number]
-    page_list.append(page)
+# Initializing spreadsheet by adding column names
+# newSheet['A1'] = 'PartNumber'
 
-for page in page_list:
-    page_content = page.extract_text()
-    print(page_content)
-    for name in re.findall(r'\b9C\S*', page_content):
-        sheet[f'A{row}'] = name
-        sheet[f'B{row}'] = '1'
-        print(f'A{row}')
-        row += 1
+# Reading address and stuff.
+receiverName = billSheet.cell(row=14, column=98).value
+receiverAddress = billSheet.cell(row=18, column=98).value
+receiverSuite = billSheet.cell(row=19, column=98).value
+receiverZipCityState = billSheet.cell(row=20, column=98).value
 
-workbook.save(filename=f'{folder}\\{fileName}.xlsx')
+for num in range(0, maxRow - 1):
+
+    # Checks for description value. If none, skip row.
+    if billSheet.cell(row=billRow, column=33).value is None:
+        billRow += 1
+        continue
+
+    # Collects all necessary information from bill
+    # If an item has a code, reads the size and quantity information, saving it in a dict (size:quantity of that size).
+    # Also reads color (color) and first two letters of color (shortColor).
+    itemCode = billSheet.cell(row=billRow, column=4).value
+    if len(itemCode) > 3:
+        billSizeRow = billRow + 5
+        billQuanRow = billSizeRow + 1
+
+        # If row with sizes isn't in place, searches for the size row below row with item code.
+        if billSheet.cell(row=billSizeRow, column=28).value is None:
+            # tempBillRow = billRow + 1
+            # while len(str(billSheet.cell(row=tempBillRow, column=28).value)) < 1:
+            #     tempBillRow += 1
+            #     billSizeRow = tempBillRow
+            #     billQuanRow = tempBillRow + 1
+            billRow += 1
+            continue
+
+        sizeQuanDict = {}
+
+        color = str(billSheet.cell(row=billSizeRow, column=6).value)
+        shortColor = color[0:2]
+
+        for tempNum in [28, 38, 42, 53, 61, 72, 83, 90, 99, 109]:
+            if billSheet.cell(row=billQuanRow, column=tempNum).value is not None:
+                size = billSheet.cell(row=billSizeRow, column=tempNum).value
+                quantity = billSheet.cell(row=billQuanRow, column=tempNum).value
+                sizeQuanDict.update({size:quantity})
+
+    sizes = sizeQuanDict.keys()
+
+    itemFullNames = []
+    itemQuantities = []
+    for sizeKey in sizes:
+        itemFullNames.append(f"{itemCode} - {shortColor} - {sizeKey}")
+        itemQuantities.append(sizeQuanDict[sizeKey])
+
+    # Outputs information into new formatted worksheet
+    for item in range(0, len(itemFullNames)):
+        newSheet.cell(row=newRow, column=1).value = itemFullNames[item]
+        newSheet.cell(row=newRow, column=2).value = itemQuantities[item]
+        newRow +=1
+
+
+    billRow += 1
+
+newWorkbook.save(filename=f'{folder}\\{fileName}.xlsx')
 
 
